1a41c6495b8b97b13103110b4d347407a910e802
Tran Huu Tin <thuutin@gmail.com>
2015-08-26 18:33:43 +0700

    enable Vibrator to works with HALs
    Change-Id: Ia67a71a6ad653e590e25bb17fc0ddbea77d68718

--- services/core/jni/com_android_server_VibratorService.cpp
+++ services/core/jni/com_android_server_VibratorService.cpp
@@ -23,27 +23,42 @@
 #include <utils/misc.h>
 #include <utils/Log.h>
 #include <hardware_legacy/vibrator.h>
-
+#include <hardware/vibrator.h>
 #include <stdio.h>
 
 namespace android
 {
+static vibrator_device_t* device;
+
+static void vibratorControlInit()
+ {
+    assert(!gVibraModule);
+    hw_module_t *gVibraModule;
+    int err;
+    err = hw_get_module(VIBRATOR_HARDWARE_MODULE_ID, (hw_module_t const**)&gVibraModule);
+    if (err) {
+      ALOGE("Couldn't load %s module (%s)", VIBRATOR_HARDWARE_MODULE_ID, strerror(-err));
+    }
+    err = vibrator_open(gVibraModule, &device);
+}
 
 static jboolean vibratorExists(JNIEnv *env, jobject clazz)
 {
-    return vibrator_exists() > 0 ? JNI_TRUE : JNI_FALSE;
+    return (device != NULL) ? JNI_TRUE : JNI_FALSE;
 }
 
 static void vibratorOn(JNIEnv *env, jobject clazz, jlong timeout_ms)
 {
     // ALOGI("vibratorOn\n");
-    vibrator_on(timeout_ms);
+    if (device)
+        device->vibrator_on(device, timeout_ms);
 }
 
 static void vibratorOff(JNIEnv *env, jobject clazz)
 {
     // ALOGI("vibratorOff\n");
-    vibrator_off();
+    if (device)
+        device->vibrator_off(device);
 }
 
 static JNINativeMethod method_table[] = {
@@ -54,6 +69,7 @@ static JNINativeMethod method_table[] = {
 
 int register_android_server_VibratorService(JNIEnv *env)
 {
+    vibratorControlInit();
     return jniRegisterNativeMethods(env, "com/android/server/VibratorService",
             method_table, NELEM(method_table));
 }
